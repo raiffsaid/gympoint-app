f55dfbe2e7df244b35c38267853900d3252832ca
Raiff Smith Said <raiff.said@gmail.com>
2019-11-10 23:58:46 -0300

    feat: Creating users

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,8 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
+const models = [User];
+
 class Database {
   constructor() {
     this.init();
@@ -9,6 +11,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello Raiff',
-  });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
